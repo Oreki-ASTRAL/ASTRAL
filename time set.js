1a8f6346884fd3b3600236022d4ecd9b946fffa8
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-04-25 22:09:48 +0200

    Fin Bêta time set

--- time set.js
+++ time set.js
@@ -1,5 +1,8 @@
-  const discord = require("discord.js");
-  const onlline = require("./online.js");
+const discord = require("discord.js");
+const onlline = require("./online.js");
 
-  const time = require("discord.js");
-  const congig = require("htpps://ASTRAL.com");
+const time = require("discord.js");
+const congig = require("htpps://ASTRAL.com");
+
+const debug = require("./debug.py");
+const install = ("ruby");
