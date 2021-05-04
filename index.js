79a93eda178026e6c7a38509eeeed60ae2506bc8
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-05-04 03:15:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const discord = require("ddiscord.js");
-const langue = require("ASTRAL.as");
-const { readJsonConfigFile } = require("typescript");
-
-$empty = fonction => wait.exe;
-
-const imports = require("https://github.com/Oreki-ASTRAL/ASTRAL");
-const exports = require("file:///C:/Users/nicol/Desktop/Bot%20maker%20V2/ASTRAL.html");
