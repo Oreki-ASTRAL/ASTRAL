3599e78f9fe969d33e61d2b285963267da5c3a33
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-08-26 10:47:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const { readJsonConfigFile } = require("typescript");
 $empty = fonction => wait.exe;
 
 const imports = require("https://github.com/Oreki-ASTRAL/ASTRAL");
-const exports = require("file:///C:/Users/nicol/Desktop/Bot%20maker%20V2/ASTRAL.html");
+const exports = require("./html.base");
